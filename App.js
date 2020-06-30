5836703cbe1fe5cda02e584c7c046e41951e935e
Balazs Danyadi <balazs.danyadi@gmail.com>
2020-06-30 17:18:13 +0200

    box style

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Header from "./Components/Header";
+import Header from "./components/Header";
+import StartScreen from "./screens/StartScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
+      <StartScreen />
     </View>
   );
 }
