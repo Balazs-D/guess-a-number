ffa50239f8ea293383da0208c55e1fb1424e3af1
Balazs Danyadi <balazs.danyadi@gmail.com>
2020-07-14 17:31:02 +0200

    Platform API

--- Components/Header.js
+++ Components/Header.js
@@ -4,24 +4,43 @@ import Colors from "../constants/colors";
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIos,
+          android: styles.headerAndroid,
+        }),
+      }}
+      // style={styles.headerBase}
+    >
       <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary,
     alignItems: "center",
     justifyContent: "center",
+  },
+  headerAndroid: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    backgroundColor: Colors.primary,
+  },
+  headerIos: {
+    borderBottomColor: "transparent",
+    borderBottomWidth: 0,
+    backgroundColor: "white",
   },
   headerTitle: {
     color: "black",
     fontSize: 18,
     fontFamily: "open-sans-bold",
+    color: Platform.OS === "ios" ? Colors.primary : "white",
   },
 });
 export default Header;
--- Components/MainButton.js
+++ Components/MainButton.js
@@ -4,31 +4,44 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  ImagePropTypes,
+  TouchableNativeFeedback,
+  Platform,
+  Dimensions,
 } from "react-native";
 import Colors from "../constants/colors";
 
 const MainButton = (props) => {
+  let ButtonComp = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    ButtonComp = TouchableNativeFeedback;
+  }
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonMainCont}>
+      <ButtonComp activeOpacity={0.6} onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComp>
+    </View>
   );
 };
 const styles = StyleSheet.create({
+  buttonMainCont: {
+    borderRadius: 25,
+    overflow: "hidden",
+  },
   button: {
     backgroundColor: Colors.primary,
     paddingVertical: 12,
     paddingHorizontal: 20,
     borderRadius: 25,
-    width: "100%",
+    width: Dimensions.get("window").width / 4,
   },
   buttonText: {
     color: "white",
     fontFamily: "open-sans-bold",
     fontSize: 18,
+    textAlign: "center",
   },
 });
 export default MainButton;
